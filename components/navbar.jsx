0578bee785b7ca43a34701b604bba39a8bec3790
itsvickyanand <vickyanandtoprock@gmail.com>
2023-01-09 13:56:22 +0530

    final

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,13 +1,20 @@
 import { useRouter } from 'next/router'
 import React from 'react'
+const style = {
+  backgroundColor: 'yellow',
+  display: 'flex',
+  gap: '30px',
+  justifyContent: 'center',
 
+  height: '60px',
+}
 const Navbar = () => {
   const router = useRouter()
   const handleClick = (route) => {
     router.push(route)
   }
   return (
-    <div>
+    <div style={style}>
       <button onClick={() => handleClick('/')}>Vicky Anand</button>
       <button onClick={() => handleClick('/projects')}>Projects</button>
       <button onClick={() => handleClick('/experience')}>Experience</button>
